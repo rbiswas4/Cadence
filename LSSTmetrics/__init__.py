9eb56a832c6417cfe0c68e94fdb36c0dde381037
R. Biswas <rbiswas4@gmail.com>
2017-05-26 00:16:41 -0700

    Added code for example_data directory as attribute 	modified:   LSSTmetrics/LightCurveMetricsUsed_ForPaper.ipynb 	modified:   LSSTmetrics/__init__.py 	modified:   gedankenLSST/__init__.py

--- LSSTmetrics/__init__.py
+++ LSSTmetrics/__init__.py
@@ -2,3 +2,6 @@ from __future__ import absolute_import
 from .efficiencyTable import *
 from .metrics import *
 from .version import __version__
+here = __file__
+basedir = os.path.split(here)[0]
+example_data = os.path.join(basedir, 'example_data')
--- gedankenLSST/__init__.py
+++ gedankenLSST/__init__.py
@@ -2,3 +2,7 @@ from __future__ import absolute_import
 from .gedankenLSST import *
 from .sninLSST import *
 from .version import __version__ 
+
+here = __file__
+basedir = os.path.split(here)[0]
+example_data = os.path.join(basedir, 'example_data')
